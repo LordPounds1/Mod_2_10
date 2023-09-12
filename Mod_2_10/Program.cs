e80b35722e5336b02bd904e8e8be8da3578d33ea
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 21:45:59 +0600

    Add project files.

---
+++ Mod_2_10/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Mod_2_10
+{
+    class Program
+    {
+        static string num_Of_year(int b)
+        {
+            if (b == 12 || b == 1 || b == 2)
+            {
+                return "Зима";
+            }
+            else if (b == 3 || b == 4 || b == 5)
+            {
+                return "Весна";
+            }
+            else if (b == 6 || b == 7 || b == 8)
+            {
+                return "Лето";
+            }
+            else if (b == 9 || b == 10 || b == 11)
+            {
+                return "Осень";
+            }
+            else
+            {
+                return "Введите число от 1 до 12";
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.Write("Введите порядковый номер месяца: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            string b = num_Of_year(a);
+            Console.WriteLine(b);
+        }
+    }
+}
